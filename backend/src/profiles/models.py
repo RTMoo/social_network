6516eebcc56e688fccb5cc40e935cb6bdd6b1dbd
RTMoo <toktarkazyramazan@gmail.com>
2025-05-27 14:09:21 +0500

    fix: все поля модели Profile не обязательны

--- backend/src/profiles/models.py
+++ backend/src/profiles/models.py
@@ -4,9 +4,9 @@ from django.core.validators import MinLengthValidator
 
 class Profile(models.Model):
     user = models.OneToOneField(to="accounts.CustomUser", on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)])
-    last_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)])
-    avatar = models.ImageField(upload_to="profile/avatars/")
+    first_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)], blank=True, null=True)
+    last_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)], blank=True, null=True)
+    avatar = models.ImageField(upload_to="profile/avatars/", blank=True, null=True)
     bio = models.TextField(max_length=512, blank=True, null=True)
-    birth_date = models.DateField()
-    country = models.CharField(max_length=2)  # библиотека django-countries
+    birth_date = models.DateField(blank=True, null=True)
+    country = models.CharField(max_length=2, blank=True, null=True)  # библиотека django-countries
