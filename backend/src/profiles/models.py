af618f2203ee4132bd52352aa3572fe1e52bd8a6
RTMoo <toktarkazyramazan@gmail.com>
2025-05-28 00:17:47 +0500

    refactor: универсальный валидатор для модели и сериализатора

--- backend/src/profiles/models.py
+++ backend/src/profiles/models.py
@@ -1,25 +1,24 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 from profiles.validators import validate_min_length_if_not_empty
 
 
 class Profile(models.Model):
     user = models.OneToOneField(to="accounts.CustomUser", on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=32, blank=True, null=True)
-    last_name = models.CharField(max_length=32, blank=True, null=True)
+    first_name = models.CharField(max_length=32, blank=True, default="")
+    last_name = models.CharField(max_length=32, blank=True, default="")
     avatar = models.ImageField(upload_to="profile/avatars/", blank=True, null=True)
     bio = models.TextField(max_length=512, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     country = models.CharField(max_length=2, blank=True, null=True)
 
     def clean(self):
-        valid, error_message = validate_min_length_if_not_empty(self.first_name)
-        if not valid:
-            raise ValidationError({"first_name": error_message})
+        validate_min_length_if_not_empty(
+            field_name="first_name", value=self.first_name, is_model_exception=True
+        )
 
-        valid, error_message = validate_min_length_if_not_empty(self.last_name)
-        if not valid:
-            raise ValidationError({"last_name": error_message})
+        validate_min_length_if_not_empty(
+            field_name="last_name", value=self.last_name, is_model_exception=True
+        )
 
     def save(self, force_insert=..., force_update=..., using=..., update_fields=...):
         self.full_clean()
--- backend/src/profiles/validators.py
+++ backend/src/profiles/validators.py
@@ -1,10 +1,15 @@
-from typing import Tuple
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.exceptions import ValidationError as SerializerValidationError
 
 
-def validate_min_length_if_not_empty(value: str) -> Tuple[bool, str]:
+def validate_min_length_if_not_empty(
+    field_name: str, value: str, is_model_exception: bool = False
+):
     min_length = 2
     error_message = "Длина значения должна быть либо пустой, либо не менее 2 символов"
 
     if value and len(value) < min_length:
-        return False, error_message
-    return True, ""
+        if is_model_exception:
+            raise DjangoValidationError({field_name: error_message})
+
+        raise SerializerValidationError({field_name: error_message})
