d9193ce509f50b0eb145aa839b2f9184ede629f3
RTMoo <toktarkazyramazan@gmail.com>
2025-05-27 14:01:04 +0500

    feat: Модель Profile

--- backend/src/profiles/models.py
+++ backend/src/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(to="accounts.CustomUser", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)])
+    last_name = models.CharField(max_length=32, validators=[MinLengthValidator(2)])
+    avatar = models.ImageField(upload_to="profile/avatars/")
+    bio = models.TextField(max_length=512, blank=True, null=True)
+    birth_date = models.DateField()
+    country = models.CharField(max_length=2)  # библиотека django-countries
