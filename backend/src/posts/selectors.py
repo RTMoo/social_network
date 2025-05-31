974f932dde61178cec364b166bd30490bae48872
RTMoo <toktarkazyramazan@gmail.com>
2025-05-31 12:34:41 +0500

    feat: получить пост по post_id #3

--- backend/src/posts/selectors.py
+++ backend/src/posts/selectors.py
@@ -1,4 +1,5 @@
 from posts.models import Post
+from rest_framework.exceptions import NotFound
 
 
 def get_user_posts(username: str):
@@ -7,3 +8,12 @@ def get_user_posts(username: str):
     )
 
     return posts
+
+
+def get_post(post_id: int) -> Post:
+    post = Post.objects.filter(id=post_id).first()
+
+    if not post:
+        raise NotFound()
+
+    return post
--- backend/src/posts/views.py
+++ backend/src/posts/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
 from posts.serializers import PostSerializer
 from posts.services import create_post
-from posts.selectors import get_user_posts
+from posts.selectors import get_user_posts, get_post
 
 
 @extend_schema(
@@ -34,5 +34,18 @@ def get_user_posts_view(request: Request, username: str):
     posts = get_user_posts(username=username)
 
     data = PostSerializer(instance=posts, many=True).data
+
+    return Response(data=data, status=status.HTTP_200_OK)
+
+
+@extend_schema(
+    responses=PostSerializer,
+)
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_post_view(request: Request, post_id: int):
+    post = get_post(post_id=post_id)
+
+    data = PostSerializer(instance=post).data
 
     return Response(data=data, status=status.HTTP_200_OK)
