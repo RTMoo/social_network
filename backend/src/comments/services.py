c04f659d004035695eb47c9717c1a7c179251c65
RTMoo <toktarkazyramazan@gmail.com>
2025-06-04 21:49:01 +0500

    fix: теперь все ответы в треде ссылаются на корневой комментарий #6

--- backend/src/comments/services.py
+++ backend/src/comments/services.py
@@ -11,13 +11,20 @@ def create_comment(data: dict[str, Any], author: CustomUser) -> Comment:
 
     parent = None
     parent_id = data.get("parent_id")
-    if parent_id is not None:
-        parent = get_comment(comment_id=parent_id)
 
-        if parent.post_id != post.id:
+    if parent_id is not None:
+        parent_comment = get_comment(comment_id=parent_id)
+
+        if parent_comment.post_id != post.id:
             raise ValidationError(
                 {"parent_id": "Родительский комментарий не принадлежит данному посту."}
             )
+
+        # если parent_comment — это ответ, берём его parent_id
+        if parent_comment.parent is not None:
+            parent = parent_comment.parent
+        else:
+            parent = parent_comment
 
     comment = Comment.objects.create(
         post=post,
