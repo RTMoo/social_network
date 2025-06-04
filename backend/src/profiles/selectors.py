14028ff8510b4213a00334a218837567d4739de8
RTMoo <toktarkazyramazan@gmail.com>
2025-06-05 00:58:53 +0500

    fix: Исправлено ошибка при получении данных профиля

--- backend/src/profiles/selectors.py
+++ backend/src/profiles/selectors.py
@@ -2,8 +2,8 @@ from rest_framework.exceptions import NotFound
 from profiles.models import Profile
 
 
-def get_profile(user_id: int) -> Profile:
-    profile = Profile.objects.filter(user_id=user_id).first()
+def get_profile(username: str) -> Profile:
+    profile = Profile.objects.filter(user__username=username).first()
 
     if not profile:
         raise NotFound(detail="Профиль не найдено.")
