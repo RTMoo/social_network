742bad3185963bd4df23f4b82ab5dc44afe76284
RTMoo <toktarkazyramazan@gmail.com>
2025-06-06 22:39:09 +0500

    feat: модель Like и сериализатор #8

--- backend/src/likes/models.py
+++ backend/src/likes/models.py
@@ -4,14 +4,18 @@ from django.core.exceptions import ValidationError
 
 class Like(models.Model):
     user = models.ForeignKey(to="accounts.CustomUser", on_delete=models.CASCADE)
-    post = models.ForeignKey(to="posts.Post", on_delete=models.CASCADE, null=True, blank=True)
-    comment = models.ForeignKey(to="comments.Comment", on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(
+        to="posts.Post", on_delete=models.CASCADE, null=True, blank=True
+    )
+    comment = models.ForeignKey(
+        to="comments.Comment", on_delete=models.CASCADE, null=True, blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = (
-            ('user', 'post'),
-            ('user', 'comment'),
+            ("user", "post"),
+            ("user", "comment"),
         )
 
     def clean(self):
