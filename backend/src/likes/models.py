b42e42a34cf14282e11e61a008fc653074171fa6
RTMoo <toktarkazyramazan@gmail.com>
2025-06-07 09:52:44 +0500

    ruff

--- backend/src/likes/models.py
+++ backend/src/likes/models.py
@@ -5,10 +5,16 @@ from django.core.exceptions import ValidationError
 class Like(models.Model):
     user = models.ForeignKey(to="accounts.CustomUser", on_delete=models.CASCADE)
     post = models.ForeignKey(
-        to="posts.Post", on_delete=models.CASCADE, null=True, blank=True
+        to="posts.Post",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
     comment = models.ForeignKey(
-        to="comments.Comment", on_delete=models.CASCADE, null=True, blank=True
+        to="comments.Comment",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
