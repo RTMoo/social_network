4bd7bbb586cb2dff1affb310eba739c84a57ebcb
RTMoo <toktarkazyramazan@gmail.com>
2025-07-05 03:09:38 +0500

    feat: добавление фронтенд-сервиса с конфигурацией Docker и обновление документации проекта

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3000,
   }
 })
